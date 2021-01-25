cfc2dfbdd761d10abd15dc4eb91fcc26457887f5
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-01-25 11:02:37 +0100

    [TST] Add mocks target
    If other library is under test and depends on this library, it will
    have now the possibility to link the target containing mocks.
    
    Fixes:
    SEOS-2087

---
+++ mocks/include/system_config.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2021, HENSOLDT Cyber GmbH
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * @brief Debug configuration
+ */
+#pragma once
+
+#define Debug_Config_STANDARD_ASSERT
+#define Debug_Config_ASSERT_SELF_PTR
+
+#define Debug_Config_LOG_LEVEL              Debug_LOG_LEVEL_NONE
